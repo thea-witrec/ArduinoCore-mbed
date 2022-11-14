1abe2976f0afbe2ab8df304e94b8ae38ac081006
Marco Marche <marco.marche@gmail.com>
2022-11-14 10:13:57 +0100

    docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# The scope of this file is splitting the build into self consistent packages for distribution
-echo "========== Configuration ==============="
+export VERSION="3.3.0"
 
-source $1
+rm -rf /tmp/mbed-os-program
+git reset --hard
+git clean -dxf
+
+cd /arduino
+mkdir mbed-os
+cd mbed-os
+git init
+git remote add origin https://github.com/ARMmbed/mbed-os.git
+git fetch --depth 1 origin 751d0cf98bb54287285103c3c5c48ae09fb7cb4c
+git checkout FETCH_HEAD
+cd /arduino/ArduinoCore-mbed
+
+git clone https://github.com/arduino/ArduinoCore-API.git ../api/
+ln -s ../../../api/api cores/arduino/api
+
+source full.variables
 
 echo $VERSION
 echo $FLAVOUR
@@ -46,12 +61,15 @@ mv _boards.txt boards.txt
 
 fi
 
+export MBED_UPDATE=0
+
 #Recompile mbed core, applying patches on origin/latest
 set +e
-./mbed-os-to-arduino -b origin/latest -a NOPE:NOPE
+./mbed-os-to-arduino -r /arduino/mbed-os -a NOPE:NOPE
+sed -i 's/uint32_t id/uintptr_t id/' /tmp/mbed-os-program/mbed-os/targets/TARGET_RASPBERRYPI/TARGET_RP2040/gpio_api.c
 set -e
 for variant in $VARIANTS; do
-./mbed-os-to-arduino $variant:$variant
+./mbed-os-to-arduino -r /arduino/mbed-os $variant:$variant
 done
 
 # Remove bootloaders not in $BOOTLOADERS list
@@ -74,8 +92,11 @@ BASE_FOLDER=`basename $PWD`
 cd ../dist
 echo "tar --exclude='*.git*' --exclude='*patches*' -cjhf ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.bz2 -C .. $BASE_FOLDER"
 tar --exclude='*.git*' --exclude='*patches*' -cjhf ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.bz2 -C .. $BASE_FOLDER
+echo "tar --exclude='*.git*' --exclude='*patches*' -czhf ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.gz -C .. $BASE_FOLDER"
+tar --exclude='*.git*' --exclude='*patches*' -czhf ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.gz -C .. $BASE_FOLDER
 if [ x$FLAVOUR == x ]; then
 mv ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.bz2 ArduinoCore-mbed-$VERSION.tar.bz2
+mv ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.gz ArduinoCore-mbed-$VERSION.tar.gz
 echo FILENAME=ArduinoCore-mbed-$VERSION.tar.bz2 > /tmp/env
 else
 echo FILENAME=ArduinoCore-mbed-$FLAVOUR-$VERSION.tar.bz2 > /tmp/env
